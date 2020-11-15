e85f15e9880d05ac1ea67253677ab35db4924b9e
Georges Bou-Issa <georges.bouissa@inmind.ai>
2020-11-15 18:00:44 +0200

    fix env file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,8 @@
 export const environment = {
   production: false,
  
-  apiKey: 'api key',
-  apiUrl: 'http://api.openweathermap.org/data/2.5/weather?id=524901&appid=729e94b46a431cf19e05673a94b1188e',
+  apiKey: '729e94b46a431cf19e05673a94b1188e',
+  apiUrl: 'http://api.openweathermap.org/data/2.5',
   
 };
 
